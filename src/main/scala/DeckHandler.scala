93114213271371d2999661fdacec44c25318f163
Cheezy <cmeier@lifetimefitness.com>
2017-08-30 14:50:47 -0600

    Implementing shuffling

--- src/main/scala/DeckHandler.scala
+++ src/main/scala/DeckHandler.scala
@@ -2,6 +2,8 @@ package cards.deck
 
 import cards.card._
 
+import scala.util.Random
+
 object Deck {
   type Deck = Seq[Card]
 }
@@ -25,10 +27,10 @@ class DeckHandler {
       cards.filter(_.getSuite == Club).size == 13)
   }
 
-  def shuffle(cards: Deck): Deck = ???
+  def shuffle(cards: Deck): Deck = Random.shuffle(cards)
 
   def deal(cards: Deck, playerCount: Int, cardsEach: Int): Map[Int, Deck] = {
-    cards.foldLeft[(Int, Map[Int, Deck])](0 -> Map.empty) {
+    shuffle(cards).foldLeft[(Int, Map[Int, Deck])](0 -> Map.empty) {
       case ((nextPlayer: Int, dealt: Map[Int, Seq[Card]]), nextCard) => {
         val psCards = dealt.getOrElse(nextPlayer, Seq.empty)
         if (psCards.size < cardsEach) ((nextPlayer + 1) % playerCount) -> (dealt + (nextPlayer -> (psCards :+ nextCard))) else
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,13 +9,10 @@ object Main {
   def main(args: Array[String]):Unit = {
     val d = new DeckHandler()
     val filledDeck: Deck = d.build
-    println(d.validate(filledDeck))
     d.deal(filledDeck, 10, 5).map(c => {
       val playerId = c._1
       val cards = c._2.map(c => c.getValue.display + c.getSuite.display)
       println(s"Player ${playerId} has ${cards}")
     })
-    val shoe = new Shoe().build(40)
-    println(shoe)
   }
 }
